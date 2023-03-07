3627e19f8ceb7d2b6d6553dcd986e3ac34f76f06
Danilo Martins <dansm86@outlook.com>
2023-03-07 13:19:30 -0300

    fix/req07-removed cls parameter from Importer
    class method

--- inventory_report/importer/importer.py
+++ inventory_report/importer/importer.py
@@ -4,5 +4,5 @@ from abc import ABC, abstractmethod
 class Importer(ABC):
     @staticmethod
     @abstractmethod
-    def import_data(cls, file_path):
+    def import_data(file_path):
         raise NotImplementedError
